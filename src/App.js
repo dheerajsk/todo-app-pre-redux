2729f4cc66e59aa25f1a5071f5c955002e5b95d6
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2023-02-16 10:17:15 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import TodoForm from "./components/ToDoForm/ToDoForm";
+import TodoList from "./components/ToDoList/ToDoList";
+
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const createTodo = (text) => {
+    setTodos([...todos, { id: todos.length + 1, text, completed: false}]);
+  };
+
+  const toggleTodo = (index)=>{
+    const list = [...todos];
+    list[index].completed = !list[index].completed;
+    setTodos(list);
+  }
+
+  return (
+    <div>
+      <TodoForm onCreateTodo={createTodo} />
+      <TodoList todos={todos} onToggle={toggleTodo} />
+    </div>
+  );
+}
+
+export default App;
